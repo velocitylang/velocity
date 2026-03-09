6d0b8c3a99772bcf796b859e0415912795b03994
Jason Creviston <the@j4w8n.com>
2026-03-09 00:13:12 -0400

    start building lexer

---
+++ src/grammar.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, PartialEq)]
+pub enum Token {
+  Number(f64),
+  Plus,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::fs;
+use std::iter::Peekable;
+use std::str::Chars;
+use crate::grammar::Token;
+
+pub mod grammar;
+
+fn get_next_token(chars: &mut Peekable<Chars>) -> Option<Token> {
+    loop {
+        match chars.peek() {
+            Some(c) if c.is_whitespace() => {
+                chars.next();
+            },
+            _ => break,
+        }
+    }
+
+    let char = chars.next();
+    match char {
+        Some('+') => Some(Token::Plus),
+        Some(c) if c.is_digit(10) => {
+            let mut num_str = String::from(c);
+
+            while let Some(&next_char) = chars.peek() {
+                if next_char.is_digit(10) {
+                    num_str.push(chars.next().unwrap());
+                } else {
+                    break;
+                }
+            }
+
+            Some(Token::Number(num_str.parse().unwrap()))
+        },
+        None => None,
+        _ => {
+            eprintln!("Error: Unknown character {:?}", char.unwrap());
+            None
+        },
+    }
+}
+
 fn main() {
-    println!("Welcome to Velocity");
+    let source_result = fs::read_to_string("./main.vl");
+
+    if let Ok(source) = source_result {
+        println!("{source}");
+        let mut tokens: Vec<Token> = Vec::new();
+
+        let mut chars = source.chars().peekable();
+
+        while let Some(next_token) = get_next_token(&mut chars) {
+            tokens.push(next_token);
+        }
+
+        println!("Found tokens: {:?}", tokens);
+    } else {
+        println!("Error reading source file");
+    }
 }
